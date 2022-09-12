723044b668d2e0e68c99a40e2eb3199eb82d1fbb
junjiecheo12 <wkcheojj@gmail.com>
2022-09-12 10:56:27 +0800

    second commit

--- server.js
+++ server.js
@@ -1,2 +1,17 @@
 require("dotenv").config();
+const log = require("debug")("holidays:server");
 const express = require("express");
+const morgan = require("morgan");
+
+const PORT = process.env.PORT ?? 3000;
+const app = express();
+
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
+  res.send({ msg: "Holidays" });
+});
+
+app.listen(PORT, () => {
+  log(`Express listing on ${PORT}`);
+});

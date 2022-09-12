7e2f2017799ebf4144bd165f82697dec02b87319
junjiecheo12 <wkcheojj@gmail.com>
2022-09-12 14:53:55 +0800

    fix

--- server.js
+++ server.js
@@ -3,8 +3,20 @@ const log = require("debug")("holidays:server");
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const mongoose = require("mongoose");
+
 const PORT = process.env.PORT ?? 3000;
 const app = express();
+const MONGO_URL = process.env.MONGO_URL;
+
+mongoose.connect(MONGO_URL);
+mongoose.connection.on("error", (err) =>
+  console.log(err.message + " is MongodB not running?")
+);
+mongoose.connection.on("disconnected", () => console.log("mongo disconnected"));
+mongoose.connection.once("open", () => {
+  console.log("connected to mongoose...");
+});
 
 app.use(morgan("dev"));
 app.use(cors());

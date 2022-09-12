3095283523b4bac2ef4aaa6481e7dc63d32db6fa
junjiecheo12 <wkcheojj@gmail.com>
2022-09-12 11:53:49 +0800

    test

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ require("dotenv").config();
 const log = require("debug")("holidays:server");
 const express = require("express");
 const morgan = require("morgan");
-
+const cors = require("cors");
 const PORT = process.env.PORT ?? 3000;
 const app = express();
 
 app.use(morgan("dev"));
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send({ msg: "Holidays" });
